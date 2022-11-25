fce2eba48a3e397c39b9e1ee570474ad848c994f
fllantada <franciscollantada@gmail.com>
2022-11-25 10:31:04 -0300

    HoursChallenge

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,17 @@ import Challenge from "./index";
 
 describe("Challenge", () => {
   it("should return the correct result", () => {
-    const result = Challenge(4);
-    expect(result).toBe(6);
+    const result = Challenge(7362);
+    expect(result).toBe("2h1m");
+  });
+
+  it("should return the correct result", () => {
+    const result = Challenge(100);
+    expect(result).toBe("1m40s");
+  });
+
+  it("should return the correct result", () => {
+    const result = Challenge(5);
+    expect(result).toBe("5s");
   });
 });
